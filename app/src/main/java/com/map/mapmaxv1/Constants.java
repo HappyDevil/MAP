efe64bd404166926c3a1d9493e6e063509c026f6
Levitsky Daniil <happydevilpro@gmail.com>
2017-02-28 09:11:55 +0300

    Auth model

---
+++ app/src/main/java/com/map/mapmaxv1/Constants.java
@@ -0,0 +1,8 @@
+package com.map.mapmaxv1;
+
+/**
+ * Created by Даниил on 27.02.2017.
+ */
+
+public class Constants {
+}
---
+++ app/src/main/java/com/map/mapmaxv1/dto/UserDTO.java
@@ -0,0 +1,8 @@
+package com.map.mapmaxv1.dto;
+
+/**
+ * Created by Даниил on 27.02.2017.
+ */
+
+public class UserDTO {
+}
